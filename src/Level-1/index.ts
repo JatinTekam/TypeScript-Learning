b1bc2fb8521859da56f5f6501ef5da25477c3fb3
JatinTekam <tekamjatin@gmail.com>
2025-12-10 12:33:29 +0530

    add level-1

---
+++ src/Level-1/index.ts
@@ -0,0 +1,31 @@
+// Intor To TypeScript
+// TypeScript Add Syntax On Top Of JavaScript
+// TypeScript Ensure Type Safety
+
+
+//Variable
+let a:number;
+// a="DemoStr" // Type 'string' is not assignable to type 'number'.
+a=10;
+
+let b:string;
+//b=100  //Type 'number' is not assignable to type 'string'.
+b="DemoStr"
+
+
+//function
+
+//Parameter 'num2' implicitly has an 'any' type.
+// function addTwoNumber(num1,num2){
+//     return num1+num2;
+// }
+
+function addTwoNumber(num1:number,num2:number):number{
+    return num1+num2;
+}
+
+//addTwoNumber("str",50); //Argument of type 'string' is not assignable to parameter of type 'number'.
+addTwoNumber(10,50);
+
+
+
